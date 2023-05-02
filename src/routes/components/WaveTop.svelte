f678721124288bbc591540566f3994b871553547
Natto <zx4545zx.zx4545zx@gmail.com>
2023-05-02 13:55:48 +0700

    backup-update

--- src/routes/components/WaveTop.svelte
+++ src/routes/components/WaveTop.svelte
@@ -1,8 +1,8 @@
 <script>
-
+import bg from "$lib/assets/bg-sm-top.png"
 </script>
 
-<div style="overflow: hidden;" class="w-full ">
+<!-- <div style="overflow: hidden;" class="w-full ">
     <svg
       preserveAspectRatio="none"
       viewBox="0 0 1200 120"
@@ -19,4 +19,8 @@
       />
       <path d="M0 0v5.63C149.93 59 314.09 71.32 475.83 42.57c43-7.64 84.23-20.12 127.61-26.46 59-8.63 112.48 12.24 165.56 35.4C827.93 77.22 886 95.24 951.2 90c86.53-7 172.46-45.71 248.8-84.81V0z" />
     </svg>
+  </div> -->
+
+  <div class="w-full overflow-hidden">
+    <img src={bg} alt="bg" class="w-full h-full object-cover">
   </div>
