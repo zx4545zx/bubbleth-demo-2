2ebe5e3b5c9e54e0ea15b0b88cc41ec2e319ce89
Natto <zx4545zx.zx4545zx@gmail.com>
2023-05-03 17:59:48 +0700

    add id #

--- src/lib/data/homeLanding.js
+++ src/lib/data/homeLanding.js
@@ -3,7 +3,7 @@ export const landingImages = [
     url: new URL("../assets/home/landing/1.jpg", import.meta.url).href,
     title: "สถานที่สวยงามที่ต้องไปเยือนซักครั้ง",
     des: "คาเฟ่สไตล์มัลดีฟส์ มีบ่อน้ำอยู่ตรงกลางเชื่อมต่อกันด้วยสะพานไม้ที่ทอดยาว และยังอีกหลายโซนให้ได้เที่ยวชม",
-    href: "/about"
+    href: "/about#zone"
   },
   {
     url: new URL("../assets/home/landing/2.jpg", import.meta.url).href,
@@ -15,7 +15,7 @@ export const landingImages = [
     url: new URL("../assets/home/landing/3.jpg", import.meta.url).href,
     title: "บรรยากาศร่มรื่น",
     des: "บรรยากาศร่มรื่น สบายๆ พนักงานอัธยาศัยดี เหมาะแก่การมาพักผ่อนหย่อนใจ",
-    href: "/about"
+    href: "/about#zone"
   },
   {
     url: new URL("../assets/home/landing/4.jpg", import.meta.url).href,
@@ -33,6 +33,6 @@ export const landingImages = [
     url: new URL("../assets/home/landing/6.jpg", import.meta.url).href,
     title: "บรรยากาศยามค่ำคืน",
     des: "ร้านเปิดตั้งแต่เวลา 10.00 น. - 21.00 น. เพื่อการรับประทานอาหารมื้อดึก สำหรับคู่รักหรือครอบครัว",
-    href: "/about"
+    href: "/about#zone"
   },
 ];
