a1762159627b127a0780ccfc830c1867aa972f6e
LAPTOP-2G06VCD7\Admin <shwiranda@gmail.com>
2023-05-08 12:43:28 +0700

    add zone text

--- src/lib/data/aboutLanding.js
+++ src/lib/data/aboutLanding.js
@@ -1,38 +1,81 @@
 export const zoneImages = [
     {
       url: new URL("../assets/about/z1.jpg", import.meta.url).href,
-      title: "โซนศาลาส่วนตัว",
-      des: "นั่งพักผ่อน มีชิงช้าให้ทำคอนเทนต์รับลมเก๋ ๆ หลับเพลิน ๆ บนเบาะบีนแบคเลย",
+      title: {
+        'th':'โซนศาลาส่วนตัว',
+        'en':'Private pavilion zone'
+      },
+      des:  {
+        'th':'นั่งพักผ่อน มีชิงช้าให้ทำคอนเทนต์รับลมเก๋ ๆ หลับเพลิน ๆ บนเบาะบีนแบคเลย',
+        'en':'Sit back and relax, there is a swing to make cool airy content, enjoy sleeping on the beanbag cushion.'
+      },
     },
     {
         url: new URL("../assets/about/z2.jpg", import.meta.url).href,
-        title: "โซนศาลา 2 ชั้นกลางน้ำ",
-        des: "รับลมกลางบ่อน้ำใหญ่ เห็นวิวทั่วร้าน มีเนตตาข่ายให้เป็นนอนเล่นถ่ายรูปเพลินๆ",
+        title:{
+            'th':'โซนศาลา 2 ชั้นกลางน้ำ',
+            'en':'Two- story pavilion in the middle of the water zone'
+          },
+        des:{
+            'th':'รับลมกลางบ่อน้ำใหญ่ เห็นวิวทั่วร้าน มีเนตตาข่ายให้เป็นนอนเล่นถ่ายรูปเพลินๆ',
+            'en':'Get the wind in the middle of the big pond. See the view throughout the restaurant. There is a net to lie down and take pictures.'
+          },
     },
     {
         url: new URL("../assets/about/z3.jpg", import.meta.url).href,
-        title: "โซนสะพานกลางน้ำ  โต๊ะกลมกลางน้ำสุดฮิต",
-        des: "ไฮไลท์ที่ใครมาก็ว้าว เหมือนได้นั่งทางข้าวแบบลอยน้ำ ลมเย็นได้ฟีลมัลดีฟสุดๆ",
+        title: {
+            'th':'โซนสะพานกลางน้ำ  โต๊ะกลมกลางน้ำสุดฮิต',
+            'en':'Bridge in the middle of the water, the most popular round table in the water zone.'
+          },
+        des: {
+            'th':'ไฮไลท์ที่ใครมาก็ว้าว เหมือนได้นั่งทานข้าวแบบลอยน้ำ ลมเย็นได้ฟีลมัลดีฟสุดๆ',
+            'en':"The highlight that anyone who comes is wow.It's like having a floating meal. The cool breeze gives the most Maldives vibes."
+          }, 
     },
     {
         url: new URL("../assets/about/z4.jpg", import.meta.url).href,
-        title: "โซนโต๊ะไม้ระเบียงข้างห้องแอร์ วิวบ่อน้ำเล็ก",
-        des: "สำหรับคนที่ไม่ชอบรอคิวนาน ว่างตรงไหนเข้าได้เลย แม้จะเห็นวิวไม่มาก แต่ก็สามารถเดินถ่ายรูปได้เรื่อยๆ",
+        title: 
+        {
+            'th':'โซนโต๊ะไม้ระเบียงข้างห้องแอร์ วิวบ่อน้ำเล็ก',
+            'en':'Wooden table zone, balcony beside the air-conditioned room, small pond view.'
+        },
+        des: {
+            'th':'สำหรับคนที่ไม่ชอบรอคิวนาน ว่างตรงไหนเข้าได้เลย แม้จะเห็นวิวไม่มาก แต่ก็สามารถเดินถ่ายรูปได้เรื่อยๆ',
+            'en':"For those who don't like to wait in line for a long time, you can stay anywhere. Although not seeing much view but can continue to take pictures."
+        },
     },
     {
         url: new URL("../assets/about/z5.jpg", import.meta.url).href,
-        title: "โซนซุ้มไข่",
-        des: "ซุ้มขนาดเล็ก สำหรับเด็กเล็ก หรือคู่รักที่ต้องการพื้นที่ ที่ให้บรรยากาศความเป็นส่วนตัว",
+        title: {
+            'th':'โซนซุ้มไข่',
+            'en':'Egg arch zone'
+        },
+        des:  {
+            'th':'ซุ้มขนาดเล็ก สำหรับเด็กเล็ก หรือคู่รักที่ต้องการพื้นที่ ที่ให้บรรยากาศความเป็นส่วนตัว',
+            'en':'A small facade for young children or couples who want a space that gives a private atmosphere.'
+        },
     },
     {
         url: new URL("../assets/about/z6.jpg", import.meta.url).href,
-        title: "โซนอ่างกุชชี่",
-        des: "อ่างกุชชี่ โซนถ่ายรูปหวานๆ ถือเป็นจุดน่าถ่ายรูปอีกหนึ่งจุดของที่นี่ ที่น่าสนใจ",
+        title:  {
+            'th':'โซนโซนอ่างกุชชี่ซุ้มไข่',
+            'en':'Gucci Basin Zone'
+        },
+        des:  {
+            'th':'อ่างกุชชี่ โซนถ่ายรูปหวานๆ ถือเป็นจุดน่าถ่ายรูปอีกหนึ่งจุดของที่นี่ ที่น่าสนใจ',
+            'en':'Gucci Basin, a sweet photo zone,This is another interesting point for taking pictures here.'
+        },
     },
     {
         url: new URL("../assets/about/z7.jpg", import.meta.url).href,
-        title: "โซนบอลลูน",
-        des: "บอลลูน โซนถ่ายรูป จุดยอดนิยมสำหรับที่นี่",
+        title:  {
+            'th':'โซนบอลลูน',
+            'en':'Balloon zone'
+        },
+        des: {
+            'th':'บอลลูน โซนถ่ายรูป จุดยอดนิยมสำหรับที่นี่',
+            'en':'Balloons, photo zones, popular spots for here'
+        },
     },
   ];
   
