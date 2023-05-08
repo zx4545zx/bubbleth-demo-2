800cbd647e14054e18d6f11e58dca1d948c1803a
LAPTOP-2G06VCD7\Admin <shwiranda@gmail.com>
2023-05-08 14:05:20 +0700

    add text aboutDes and foodDes

--- src/lib/data/aboutDes.js
+++ src/lib/data/aboutDes.js
@@ -1,27 +1,27 @@
 export const slideText = {
 	des: {
 		th: "คาเฟ่สไตล์อเมริกันอินเดียนแดง ให้ความรู้สึกสดชื่น สีเขียวน้ำตาล เป็นธรรมชาติ ผสมผสานกับความ Beachy และ น้ำสี Turquoise จนได้ขนานนามว่า Mini Maldives Thailand",
-		en: ''
+		en: 'American Indian style cafe Gives a feeling of freshness, green, brown, and natural, combined with Beachy and Turquoise water until dubbed Mini Maldives Thailand.'
 	},
   title: {
     th: "ที่มาของสไตล์อินเดียนแดง",
-    en: ""
+    en: "origin of indian style"
   },
   titleDes: {
-    th: "เกิดมาจากลุคสาวละตินผมยาวผิดแทนและมีความเซกซี่ของตัวการ์ตูน Pocahontas ที่มีลักษณะคล้ายกับเจ้าของร้าน จึงเนรมิตสไตล์การตกแต่งร้าน ยูนิฟอมของพนักงานให้มีความเป็นสีน้ำตาลแนวคนป่า มีพรอพเป็นขนนกตกแต่ง และบริเวณรอบ ๆ คาเฟ่ เราจะใช้ความเป็นธรรมชาติมาทำให้สวยงาม",
-    en: ""
+    th: "เกิดมาจากลุคสาวละตินผมยาวผิวแทนและมีความเซ็กซี่ของตัวการ์ตูน Pocahontas ที่มีลักษณะคล้ายกับเจ้าของร้าน จึงเนรมิตสไตล์การตกแต่งร้าน ยูนิฟอมของพนักงานให้มีความเป็นสีน้ำตาลแนวคนป่า มีพรอพเป็นขนนกตกแต่ง และบริเวณรอบ ๆ คาเฟ่ เราจะใช้ความเป็นธรรมชาติมาทำให้สวยงาม",
+    en: "Inspired by the long-haired, tanned and sexy Latino look of the Pocahontas character that resembles a restaurant owner, Therefore creating a restaurant decoration style uniforms of restaurant staff to be brown in the concept of wild people. There are props decorated with feathers and around the cafe, we have used nature to make it beautiful."
   },
   locationA: {
     th: "170 ซ.เจริญท่า ต.บางเตย",
-    en: ""
+    en: "170 Charoen Tha Lane, Bang Toei Sub-district"
   },
   locationB: {
     th: "อ.สามพราน นครปฐม 73170",
-    en: ""
+    en: "Sampran District, Nakhon Pathom, 73170"
   },
   time: {
     th: "เปิด-ปิด : 10:00 น. - 21:00 น.",
-    en: ""
+    en: "Open : 10:00 a.m. - 9:00 p.m."
   },
   tel: {
     th: "065-727-6888",
@@ -29,6 +29,6 @@ export const slideText = {
   },
   button: {
     th: 'ติดตามข่าวสาร หรือจองล่วงหน้า ได้ที่',
-    en: ""
+    en: "Click here to follow the news or book in advance."
   }
 };
