f7e0100721136f04350ec055c55a9326591e6ec0
Anuntaya <anuntaya.nia2000@gmail.com>
2023-05-08 14:02:19 +0700

    text en home page

--- src/lib/data/homeDes.js
+++ src/lib/data/homeDes.js
@@ -1,10 +1,10 @@
 export const slideText = {
 	welcome: {
 		th: 'ยินดีต้อนรับ',
-		en: ''
+		en: 'Welcome to Bubble in the Forest'
 	},
 	des: {
 		th: 'คาเฟ่ และร้านอาหาร สไตล์มัลดีฟส์สุดเก๋ ที่มีเรือนไม้ลอยน้ำ',
-		en: ''
+		en: 'Cafe and restaurants, chic Maldives style with the floating wooden house.A beautiful place to visit once Maldives-style cafe there is a pond in the middle connected by a long wooden bridge. and many zones to visit.'
 	}
 };
