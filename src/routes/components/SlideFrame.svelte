6b6ea8b565533e580e2b295b68a210bf75f6d2be
Natto <zx4545zx.zx4545zx@gmail.com>
2023-05-08 17:21:10 +0700

    ♻️ refactor frame and wave

--- src/routes/components/SlideFrame.svelte
+++ src/routes/components/SlideFrame.svelte
@@ -1,59 +1,64 @@
 <script>
-	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
-	import frame from '$lib/assets/frame.png';
-	import defaultImg from '$lib/assets/home/slide/0.jpg';
-
-	let options = {
-		width: '100%',
-		gap: '1rem',
-		type: 'loop',
-		autoplay: true,
-		perPage: 1,
-		pagination: false,
-		speed: 1000
-	};
-
-	export let images = [defaultImg];
+  import { Splide, SplideSlide } from "@splidejs/svelte-splide";
+  import frame from "$lib/assets/frame.png";
+  import defaultImg from "$lib/assets/home/slide/0.jpg";
+  let options = {
+    width: "100%",
+    gap: "1rem",
+    type: "loop",
+    autoplay: true,
+    perPage: 1,
+    pagination: false,
+    speed: 1000,
+  };
+  export let images = [defaultImg];
 </script>
 
 <div class="container mx-auto -mt-8 sm:mt-auto">
-	<Splide {options}>
-		{#each images as img}
-			<SplideSlide>
-				<div class="relative">
-					<div class="relative">
-						<div class="absolute left-0 w-full h-full top-4 sm:top-1" style="z-index: -1;">
-							<img src={img} alt="slimg" class="mx-auto mt-6 slide-img sm:mt-9 lg:mt-16 h-4/6" />
-						</div>
-						<img src={frame} alt="frame-img" class="mx-auto frame-img" />
-					</div>
-				</div>
-			</SplideSlide>
-		{/each}
-	</Splide>
+  <Splide {options}>
+    {#each images as img}
+      <SplideSlide>
+        <div class="relative">
+          <div class="relative">
+            <div
+              class="absolute left-0 w-full h-full top-4 sm:top-1 lg:-top-3 xl:-top-1 2xl:top-0"
+              style="z-index: -1;"
+            >
+              <img
+                src={img}
+                alt="slimg"
+                class="mx-auto mt-6 slide-img sm:mt-9 lg:mt-16 h-4/6"
+              />
+            </div>
+            <img src={frame} alt="frame-img" class="mx-auto frame-img" />
+          </div>
+        </div>
+      </SplideSlide>
+    {/each}
+  </Splide>
 </div>
 
 <style>
-	.slide-img {
-		width: 80%;
-		max-width: 65rem;
-		object-fit: cover;
-		object-position: center;
-		overflow: hidden;
-	}
-	@media (max-width: 500px) {
-		.slide-img {
-			margin-top: 1rem;
-		}
-	}
-	@media (max-width: 425px) {
-		.frame-img {
-			height: 14rem;
-		}
-	}
-	@media (max-width: 300px) {
-		.slide-img {
-			margin-top: 1.5rem;
-		}
-	}
+  .slide-img {
+    width: 80%;
+    max-width: 65rem;
+    object-fit: cover;
+    object-position: center;
+    overflow: hidden;
+  }
+  @media (max-width: 500px) {
+    .slide-img {
+      margin-top: 1rem;
+    }
+  }
+  @media (max-width: 425px) {
+    .frame-img {
+      height: 14rem;
+    }
+  }
+  @media (max-width: 300px) {
+    .slide-img {
+      margin-top: 1.5rem;
+    }
+  }
 </style>
