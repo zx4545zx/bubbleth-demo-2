d00d744558efabfd36f8b7e778ce713034eb98a2
Natto <zx4545zx.zx4545zx@gmail.com>
2023-05-08 13:03:22 +0700

    update text

--- src/lib/data/eventLanding.js
+++ src/lib/data/eventLanding.js
@@ -5,11 +5,11 @@ export const landingImages = [
     url: new URL("../assets/event/landing/1.jpg", import.meta.url).href,
     title:{
       'th':'25 December 2023 - วันคริสต์มาส',
-      'en':'25 December 2023 - Christmas Day'
+      'eng':'25 December 2023 - Christmas Day'
     } ,
     des:{
       'th':'เทศกาลแห่งความสุข พร้อมกับกิจกรรมสนุกสนาน พร้อมของขวัญ และโปรโมชั่นอีกมากมาย',
-      'en':'Happy festival with fun activities with gifts and promotions'
+      'eng':'Happy festival with fun activities with gifts and promotions'
     }, 
     slide: cm
   },
@@ -17,11 +17,11 @@ export const landingImages = [
     url: new URL("../assets/event/landing/2.jpeg", import.meta.url).href,
     title:{
       'th':'13 April 2023 - วันสงกรานต์',
-      'en':'13 April 2023 - Songkran Festival'
+      'eng':'13 April 2023 - Songkran Festival'
     },
     des:{
       'th':'ชุ่มฉำ่ไปกับเทศกาลสงกรานต์ พร้อมกับอาหารอร่อยๆ บรรยากาศดีๆ ได้ที่นี่',
-      'en':'Lush with the Songkran festival. Along with delicious food, good atmosphere here'
+      'eng':'Lush with the Songkran festival. Along with delicious food, good atmosphere here'
     } ,
     slide: sk
   },
