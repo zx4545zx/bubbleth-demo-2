429e78bddab868700aa04eea010c401be24e4303
wiran <shwiranda@gmail.com>
2023-05-08 15:01:13 +0700

    cc

--- src/lib/data/foodDes.js
+++ src/lib/data/foodDes.js
@@ -3,5 +3,5 @@ export const slideText = {
 		th: "ยุคโซเชียลมีเดีย ไม่ว่าจะกินอะไร Camera eat first เสมอ ทุกรุ่นอายุไปตั้งแต่วัยรุ่นถึงวัยผู้ใหญ่ เพราะฉะนั้น การตกแต่งของเรา เราใส่ใจทุกรายละเอียด การตกแต่งจะสอดคล้องกับสไตล์ของร้าน ใช้จานไม้ในการเสิร์ฟทุกจาน การตกแต่งที่ดึงดูดคนให้ถ่ายรูป มีเมนู D.I.Y ย้อนวัย ทำเมนูที่สร้างความทรงจำ สร้างความประทับใจ เมื่อมาทานอาหารร้านเรา จะได้ความอิ่ม อร่อยและความทรงจำกลับบ้าน",
 		en: "Social media era No matter what you eat, Camera always eats first. Every generation from teenagers to adults. We pay attention to every detail. The decoration will be consistent with the style of the restaurant. Use wooden plates to serve every dish. Decorations that attract people to take pictures. There is a D.I.Y menu to bring back the ages. Create a menu that creates memories. impress when coming to eat at our restaurant will get full Delicious and memories to take home"
 	}
-	
+	    
 };
