2ab3288ddcfcc825f8abb50cde5f881f69b4af5d
Natto <zx4545zx.zx4545zx@gmail.com>
2023-05-09 15:13:21 +0700

    commit

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,12 +1,12 @@
 export const prerender = true;
 
-// import { slideImages } from '$lib/data/homeSlide';
-// import { landingImages } from '$lib/data/homeLanding';
+import { slideImages } from '$lib/data/homeSlide';
+import { landingImages } from '$lib/data/homeLanding';
 
-// /** @type {import('./$types').PageLoad} */
-// export function load() {
-// 	return {
-//     landingImages,
-//     slideImages 
-//   };
-// }
+/** @type {import('./$types').PageLoad} */
+export function load() {
+	return {
+    landingImages,
+    slideImages 
+  };
+}
